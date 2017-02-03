6cdb5a645334f2eda987cba0d16f8d6e0d39b1ed
urielcaire <uriel.caire@gmail.com>
2017-02-03 12:57:50 -0200

    My js cookie manager

---
+++ jscookie.js
@@ -0,0 +1,38 @@
+/**
+*						JsCookie
+*	A simple way to create cookies with javascript!
+*	@urielcaire
+	github: https://github.com/urielcaire/jscookie
+*/
+
+function createCookie(name, value, days){
+	var expires = "";
+	if(days){
+		var time = new Date();
+		time.setTime(time.getTime()+(days*24*60*60*1000));
+	}
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function getCookie(name){
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return "";
+}
+
+function deleteCookie(name) {
+	createCookie(name,"",-1);
+}
+
+function checkCookie(name){
+	var check = getCookie(name);
+	if(check != "")
+		alert('cookie ativo');
+	else
+		alert('cookie inativo');
+}
